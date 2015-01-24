4acfdba75524fac2985657b1081155c89db5cda4
Matt Gibson <downrightlies@gmail.com>
2015-01-24 16:59:30 +0000

    Add a bit of code to inject the env vars from the application config into the reque scheduler process.
    These are not set on the instance, as there may be more than one application running on the instance, so this is the only way to get access to them if your app depends on them.

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -7,7 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-template "/etc/init/resque-scheduler.conf" do
-  source "resque-scheduler.conf.erb"
-  mode "0755"
+node[:deploy].each do |application, deploy|
+
+  template "/etc/init/resque-scheduler-#{application}.conf" do
+
+    escaped_env_vars = OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    env_string = escaped_env_vars.collect { |key, value| %Q{#{key}="#{value}"} }.join(' ')
+
+    source "resque-scheduler.conf.erb"
+    variables({
+                  :env_vars => env_string
+              })
+    mode "0755"
+  end
+
 end
