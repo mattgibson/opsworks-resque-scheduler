b5af5bcf626058bb0a3db8fbdb3d9df4277bff91
Matt Gibson <downrightlies@gmail.com>
2015-01-24 19:15:47 +0000

    Fix restart command

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,4 +1,7 @@
-service 'resque-scheduler' do
-  action [:stop, :start]
-  provider Chef::Provider::Service::Upstart
+node[:deploy].each do |application, _|
+
+  service "resque-scheduler-#{application}" do
+    action [:stop, :start]
+    provider Chef::Provider::Service::Upstart
+  end
 end
